d7e40cc04b40bf6bd46a8a8c6c25b873cb1091d7
janvi-goyani <janvigoyani027@gmail.com>
2023-04-30 23:01:28 +0530

    Add Category

---
+++ app/src/main/java/com/hinfo/allgames/CategoryModel.kt
@@ -0,0 +1,17 @@
+package com.hinfo.allgames
+
+class CategoryModel {
+
+    lateinit var key:String
+    lateinit var name:String
+
+
+    constructor(key: String, name: String) {
+        this.key = key
+        this.name = name
+    }
+
+    constructor() {
+
+    }
+}
--- app/src/main/java/com/hinfo/allgames/MainActivity.kt
+++ app/src/main/java/com/hinfo/allgames/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.hinfo.allgames
 
+import android.R
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
@@ -20,8 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    lateinit var dbRef : DatabaseReference
-
+    lateinit var dbRef: DatabaseReference
+    var cateList = ArrayList<CategoryModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -29,7 +33,27 @@ class MainActivity : AppCompatActivity() {
 
         dbRef = FirebaseDatabase.getInstance().reference
 
-        dbRef.root.child("Images").addValueEventListener(object :ValueEventListener{
+        dbRef.root.child("Category").addValueEventListener(object : ValueEventListener {
+            @SuppressLint("ResourceAsColor")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                cateList.clear()
+                for (data in snapshot.children) {
+                    var model = data.getValue(CategoryModel::class.java)
+                    binding.tabCategory.addTab(binding.tabCategory.newTab().setText(model?.name))
+                    cateList.add(model!!)
+                }
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+
+        dbRef.root.child("Images").addValueEventListener(object : ValueEventListener {
             @SuppressLint("ResourceAsColor")
             override fun onDataChange(snapshot: DataSnapshot) {
                 var list = ArrayList<ImageModel>()
@@ -38,12 +62,16 @@ class MainActivity : AppCompatActivity() {
                     list.add(model!!)
                 }
 
-                binding.rcvWallpapers.layoutManager = GridLayoutManager(this@MainActivity,3)
-                var adapter= ImagesAdapter{ data->
+                binding.rcvWallpapers.layoutManager = GridLayoutManager(this@MainActivity, 3)
+                var adapter = ImagesAdapter { data ->
 
                     var dialog = Dialog(this@MainActivity)
                     dialog.window?.setBackgroundDrawable(ColorDrawable(android.R.color.transparent))
-                    var bin = ImageDialogBinding.inflate(LayoutInflater.from(this@MainActivity),null,false)
+                    var bin = ImageDialogBinding.inflate(
+                        LayoutInflater.from(this@MainActivity),
+                        null,
+                        false
+                    )
                     dialog.setContentView(bin.root)
 
                     Glide.with(this@MainActivity).load(data.image).into(bin.imgPhoto)
